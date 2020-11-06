38f0919a0b11264718b69833e6593853305170a9
Pete Kneller <pete@petekneller.net>
2020-11-06 14:59:03 +0000

    Adds instructions/comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions provides a caching/memoization feature for matrices
+## and their inverses.
 
-## Write a short comment describing this function
+
+## Creates a wrapped matrix
+## 
+## The matrix wrapped by the returned object can be accessed and updated via
+## the `get` and `set` elements. Other elements of the object should be 
+## considered internal and not used directly.
 
 makeCacheMatrix <- function(x = matrix()) {
   mat <- x
@@ -23,7 +28,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Solves for the inverse of the provided (wrapped) matrix
+##
+## Should the inverse have been previously calculated and no change to the 
+## wrapped matrix made then a cached copy of the inverse will be returned,
+## improving performance for large matrices
 
 cacheSolve <- function(x, ...) {
   if (is.null(x$getInv())) {
